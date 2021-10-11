9a2946dee487ad7fe657c0ef3d2a334f325c7aa6
ehong <eungitoto@gmail.com>
2021-10-11 17:44:02 +0900

    modify predict.py for deployment 2nd commit

--- analyzer/predictor/utils/plots.py
+++ analyzer/predictor/utils/plots.py
@@ -18,7 +18,7 @@ def plot(title, xlabel, ylabel, x, y, save_path):
     plt.savefig(save_path)
 
 
-def plot_result(observed_df, test_df, predicted_cases, col, window_size, ahead, show_observed_cases):
+def plot_eval_result(observed_df, test_df, predicted_cases, col, window_size, ahead, show_observed_cases):
 
     plt.figure(figsize=(12, 8))
     plt.title(f'Prediction Graph in {col}')
@@ -28,12 +28,27 @@ def plot_result(observed_df, test_df, predicted_cases, col, window_size, ahead, 
     if show_observed_cases:
         plt.plot(observed_df.index, observed_df[col], label='Observed Daily Cases')                
         plt.plot(test_df.index, test_df[col], label='True Cases')        
-        plt.plot(test_df.index[(window_size + 1):len(test_df) - 1], predicted_cases, label='Predicted Cases')        
+        plt.plot(test_df.index[(window_size + 1):len(test_df) + 1 - ahead], predicted_cases, label='Predicted Cases')        
         plt.legend()
         plt.savefig(os.path.join('runs', f'{col}_observation+prediction.png'))  
     
     else:
         plt.plot(test_df.index, test_df[col], label='True Cases')        
-        plt.plot(test_df.index[(window_size + 1):len(test_df) - 1], predicted_cases, label='Predicted Cases')        
+        plt.plot(test_df.index[(window_size + 1):len(test_df) + 1 - ahead], predicted_cases, label='Predicted Cases')        
         plt.legend()
         plt.savefig(os.path.join('runs', f'{col}_prediction.png'))  
+
+
+def plot_inference_result(observed_df, col, future, predicted_cases):
+    
+    plt.figure(figsize=(12, 8))
+    plt.title(f'Prediction Graph in {col}')
+    plt.xlabel('timestamp')
+    plt.ylabel('number of outbreaks')
+    
+    plt.plot(observed_df.index, observed_df[col], label='Observed Daily Cases')
+    plt.plot(future, predicted_cases, label='Predicted Cases')
+    plt.plot()
+    plt.legend()
+    plt.savefig(os.path.join('runs', f'{col}_prediction.png'))
+
